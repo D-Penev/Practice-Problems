a0840654e0a5ebf25d116224ff3ce3311d2d920e
Dimitar Penev <predator131@mail.bg>
2020-09-16 14:34:18 +0300

    Improve solution to apples and oranges

--- Python-Things/Hacker_Rank/Problem-Solving/Apple_And_Orange.py
+++ Python-Things/Hacker_Rank/Problem-Solving/Apple_And_Orange.py
@@ -1,2 +1,14 @@
-def apple_and_orange():
-    pass
+def apples_and_oranges():
+   samHouseCoordinates = list(map(int, input().split()))
+   appleAndOrangeTreesCoordinates = list(map(int, input().split()))
+   appleAndOrangeCount = list(map(int, input().split()))
+   applesDistance = list(map(int, input().split()))
+   orangesDistance = list(map(int, input().split()))
+
+   print(len([x + appleAndOrangeTreesCoordinates[0] for x in applesDistance
+              if x + appleAndOrangeTreesCoordinates[0] >= samHouseCoordinates[0] and x + appleAndOrangeTreesCoordinates[0] <= samHouseCoordinates[1]]))
+
+   print(len([y + appleAndOrangeTreesCoordinates[1] for y in orangesDistance if y + appleAndOrangeTreesCoordinates[1]
+              >= samHouseCoordinates[0] and y + appleAndOrangeTreesCoordinates[1] <= samHouseCoordinates[1]]))
+
+apples_and_oranges()
