4f465e823a1325b346ed6bfacdc2754e0635d1c3
Dimitar Penev <predator131@mail.bg>
2020-10-02 15:06:54 +0300

    Refactor solution to 05

--- Python-Things/Tasks-At-Work-Completed/Python-OOP/Basic-Classes/Programmer.py
+++ Python-Things/Tasks-At-Work-Completed/Python-OOP/Basic-Classes/Programmer.py
@@ -9,8 +9,9 @@ class Programmer():
         if self.language == language:
             self.skills += skills_earned
             return  f'{self.name} watched {course_name}'
-        else:
-            return  f'{self.name} does not know {language}'
+
+
+        return  f'{self.name} does not know {language}'
 
     def change_language(self, new_language, skills_needed):
         if self.skills >= skills_needed and self.language != new_language:
