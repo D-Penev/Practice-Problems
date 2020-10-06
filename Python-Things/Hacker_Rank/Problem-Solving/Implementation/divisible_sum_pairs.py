7762190a9819a6f3ae3ca34864f9a78e95e3a152
Dimitar Penev <predator131@mail.bg>
2020-10-06 15:14:59 +0300

    Remove unnecessary variable from solution

--- Python-Things/Hacker_Rank/Problem-Solving/Implementation/divisible_sum_pairs.py
+++ Python-Things/Hacker_Rank/Problem-Solving/Implementation/divisible_sum_pairs.py
@@ -3,7 +3,6 @@ def divisible_sum_pairs():
     numbers = list(map(int, input().split()))
     pair_counter = 0
     all_pairs = {x: x for x in range(0, nums[0])}
-    curr_arr_index = 0
     all_values = list(all_pairs.values())
     for i in range(nums[0]):
        for j in range(len(all_values)):
